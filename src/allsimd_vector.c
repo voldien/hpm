06d2731857b0fc3db68a21a347c1c9ea75abda05
voldie <voldiekami@gmail.com>
2017-05-01 15:29:06 +0200

    Minor improvements.

--- src/allsimd_vector.c
+++ src/allsimd_vector.c
@@ -9,52 +9,63 @@
 
 
 
-
 HPM_IMP(float, hpm_vec4_dotfv, const hpmvec4f* larg, const hpmvec4f* rarg){
 	const hpmvec4f lvMult = *larg * *rarg;
 
-	/**/
-	hpmvec4f lvTemp = _mm_shuffle_ps( lvMult, lvMult, _MM_SHUFFLE( 3, 2, 1, 0 ) );
-	lvTemp = _mm_add_ps( lvTemp, lvMult );
-	hpmvec4f lvTemp2 = _mm_shuffle_ps(lvTemp, lvTemp, _MM_SHUFFLE( 2, 3, 0, 1 ) );
+#ifdef __SSE3__
+	hpmvec4f lvMultp = _mm_hadd_ps(lvMult, lvMult);
+	lvMultp = _mm_hadd_ps(lvMultp, lvMultp);
+	return _mm_cvtss_f32(lvMultp);
+#endif
 
+	/*	Sum each components in the vector.	*/
+	hpmvec4f lvTemp = _mm_shuffle_ps( lvMult, lvMult, _MM_SHUFFLE( 2, 3, 0, 1 ) );	/*	{ x , y, z , w} => { y, x, w, z }	*/
+	lvTemp = _mm_add_ps( lvTemp, lvMult );											/*	{ x + y, y + x, w + z, w + z }	*/
+	hpmvec4f lvTemp2 = _mm_shuffle_ps(lvTemp, lvTemp, _MM_SHUFFLE( 0, 1, 3, 2 ) );	/*	{ w + z, w + z, x + y, y + x }	*/
+	lvTemp = _mm_add_ps( lvTemp, lvTemp2 );											/*	{ w + z + x + y, ... , w + z + x + y }	*/
 
-	/**/
-	return _mm_cvtss_f32 ( _mm_add_ps( lvTemp, lvTemp2 ) );
+	return _mm_cvtss_f32 ( lvTemp );	/*	Return first element.	*/
 }
 
 HPM_IMP(float, hpm_vec4_lengthfv, const hpmvec4f* arg){
-	const hpmvec4f lvMult = (*arg) * (*arg);
+	hpmvec4f lvMult = (*arg) * (*arg);
 
-	/*	Sum	*/
-	hpmvec4f lvTemp = _mm_shuffle_ps( lvMult, lvMult, _MM_SHUFFLE( 3, 2, 1, 0 ) );
-	lvTemp = _mm_add_ps( lvTemp, lvMult );
-	hpmvec4f lvTemp2 = _mm_shuffle_ps(lvTemp, lvTemp, _MM_SHUFFLE( 2, 3, 0, 1 ) );
 
-	/**/
-	return _mm_cvtss_f32(_mm_sqrt_ss(_mm_add_ps( lvTemp, lvTemp2 )));
+#ifdef __AVX__	/*	TODO add to the rest of the functions.	*/
+	hpmvec8f lvMultp = _mm256_hadd_ps(
+			_mm256_castps128_ps256(lvMult),
+			_mm256_castps128_ps256(lvMult) );
+	return _mm_cvtss_f32 ( _mm256_castps256_ps128( _mm256_sqrt_ps( lvMultp ) ) );
+#elif defined(__SSE3__)
+	hpmvec4f lvMultp = _mm_hadd_ps(lvMult, lvMult);
+	lvMult = _mm_hadd_ps(lvMultp, lvMultp);
+	return _mm_cvtss_f32(_mm_sqrt_ss( lvMult ) );
+#endif
+
+	/*	Sum each components in the vector.	*/
+	hpmvec4f lvTemp = _mm_shuffle_ps( lvMult, lvMult, _MM_SHUFFLE( 2, 3, 0, 1 ) );	/*	{ x , y, z , w} => { y, x, w, z }	*/
+	lvTemp = _mm_add_ps( lvTemp, lvMult );											/*	{ x + y, y + x, w + z, w + z }	*/
+	hpmvec4f lvTemp2 = _mm_shuffle_ps(lvTemp, lvTemp, _MM_SHUFFLE( 0, 1, 3, 2 ) );	/*	{ w + z, w + z, x + y, y + x }	*/
+	lvTemp = _mm_add_ps( lvTemp, lvTemp2 );											/*	{ w + z + x + y, ... , w + z + x + y }	*/
+	const hpmvec4f sqr = _mm_sqrt_ss( lvTemp);
+
+	return _mm_cvtss_f32 ( sqr );	/*	Return first element.	*/
 }
 
 HPM_IMP( float, hpm_vec4_lengthsqurefv, const hpmvec4f* arg){
 	const hpmvec4f lvMult = (*arg) * (*arg);
 
-	/*	Sum	*/
-	hpmvec4f lvTemp = _mm_shuffle_ps( lvMult, lvMult, _MM_SHUFFLE( 3, 2, 1, 0 ) );
-	lvTemp = _mm_add_ps( lvTemp, lvMult );
-	hpmvec4f lvTemp2 = _mm_shuffle_ps(lvTemp, lvTemp, _MM_SHUFFLE( 2, 3, 0, 1 ) );
+	/*	Sum each components in the vector.	*/
+	hpmvec4f lvTemp = _mm_shuffle_ps( lvMult, lvMult, _MM_SHUFFLE( 2, 3, 0, 1 ) );	/*	{ x , y, z , w} => { y, x, w, z }	*/
+	lvTemp = _mm_add_ps( lvTemp, lvMult );											/*	{ x + y, y + x, w + z, w + z }	*/
+	hpmvec4f lvTemp2 = _mm_shuffle_ps(lvTemp, lvTemp, _MM_SHUFFLE( 0, 1, 3, 2 ) );	/*	{ w + z, w + z, x + y, y + x }	*/
+	lvTemp = _mm_add_ps( lvTemp, lvTemp2 );											/*	{ w + z + x + y, ... , w + z + x + y }	*/
 
-	/**/
-	return _mm_cvtss_f32(_mm_add_ps( lvTemp, lvTemp2 ) );
+	return _mm_cvtss_f32 (lvTemp);	/*	Return first element.	*/
 }
 
 
-
-
-
-
-
 HPM_IMP( void, hpm_vec3_crossproductfv, const hpmvec3f* v1, const hpmvec3f* v2, hpmvec3f* out){
-
 
 	const hpmvec3f tmp1 = _mm_shuffle_ps(*v1, *v1, _MM_SHUFFLE(3, 0, 2, 1));	/*	{x, y, z, w } => {y, z, x, w }*/
 	const hpmvec3f tmp2 = _mm_shuffle_ps(*v2, *v2, _MM_SHUFFLE(3, 1, 0, 2));	/*	{x, y, z, w } => {z, x, y, w }*/
--- src/avx_matrix.c
+++ src/avx_matrix.c
@@ -21,18 +21,18 @@ HPM_IMP(void, hpm_mat4x4_multiply_mat4x4fv, const hpmvec4x4f_t larg, const hpmve
 	int i;
 
 	for(i = 0; i < 4; i++){
-		const hpmvec4f brod1 = _mm_set1_ps(larg[i][0]);
-		const hpmvec4f brod2 = _mm_set1_ps(larg[i][1]);
-		const hpmvec4f brod3 = _mm_set1_ps(larg[i][2]);
-		const hpmvec4f brod4 = _mm_set1_ps(larg[i][3]);
+		const hpmvec4f brod1 = _mm_set1_ps(rarg[i][0]);
+		const hpmvec4f brod2 = _mm_set1_ps(rarg[i][1]);
+		const hpmvec4f brod3 = _mm_set1_ps(rarg[i][2]);
+		const hpmvec4f brod4 = _mm_set1_ps(rarg[i][3]);
 
 		const hpmvec4f row = _mm_add_ps(
 					_mm_add_ps(
-						_mm_mul_ps(brod1, rarg[0]),
-						_mm_mul_ps(brod2, rarg[1])),
+						_mm_mul_ps(brod1, larg[0]),
+						_mm_mul_ps(brod2, larg[1])),
 						_mm_add_ps(
-							_mm_mul_ps(brod3, rarg[2]),
-							_mm_mul_ps(brod4, rarg[3])));
+							_mm_mul_ps(brod3, larg[2]),
+							_mm_mul_ps(brod4, larg[3])));
 		out [i] = row;
 
 	}
@@ -51,14 +51,17 @@ HPM_IMP(void, hpm_mat4x4_multiply_mat4x4fv, const hpmvec4x4f_t larg, const hpmve
 }
 
 HPM_IMP( void, hpm_mat4x4_multiply_mat1x4fv, const hpmvec4x4f_t larg, const hpmvec4f* rarg, hpmvec4f* output){
-
-
+	/*	Unfold later.	*/
+	(*output)[0] = HPM_CALLLOCALFUNC(hpm_vec4_dotfv)(&larg[0], rarg);
+	(*output)[1] = HPM_CALLLOCALFUNC(hpm_vec4_dotfv)(&larg[1], rarg);
+	(*output)[2] = HPM_CALLLOCALFUNC(hpm_vec4_dotfv)(&larg[2], rarg);
+	(*output)[3] = HPM_CALLLOCALFUNC(hpm_vec4_dotfv)(&larg[3], rarg);
 }
 
 
 HPM_IMP(void, hpm_mat4x4_identityfv, hpmvec4x4f_t mat){	/*	TODO fix if it can be optimized even further.*/
-	const hpmvec8f row0 = {1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f};
-	const hpmvec8f row1 = {0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+	const hpmvec8f row0 = { 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f };
+	const hpmvec8f row1 = { 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f };
 	hpmmat4uf* mat8 = mat;
 	mat8->oc[0] = row0;
 	mat8->oc[1] = row1;
