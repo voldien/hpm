e6e280ba993d00733364028f75e0463670aa9563
Voldie <voldiekami@gmail.com>
2017-06-02 12:26:01 +0200

    Added documentation comments and created first revision of quaternion multiplication implementation.

--- src/sse/all_sse_quaternion.c
+++ src/sse/all_sse_quaternion.c
@@ -3,48 +3,40 @@
 #include<immintrin.h>
 #   ifdef HPM_VC
 #      include<intrin.h>
-#	elif defined(HPM_GNUC)
+#	elif defined(HPM_GNUC) || defined(HPM_CLANG)
 #		include<x86intrin.h>
 #   endif
 
 
 HPM_IMP(void, hpm_quat_multi_quatfv, const hpmquatf* larg, const hpmquatf* rarg, hpmquatf* out){
 
-	hpmquatf wzyx = _mm_shuffle_ps(*larg, *larg, _MM_SHUFFLE(0,1,2,3) );
-	hpmquatf baba = _mm_shuffle_ps(*rarg, *rarg, _MM_SHUFFLE(0,1,0,1) );
-	hpmquatf dcdc = _mm_shuffle_ps(*rarg, *rarg, _MM_SHUFFLE(2,3,2,3) );
-
-	/* variable names below are for parts of componens of result (X,Y,Z,W) */
-	/* nX stands for -X and similarly for the other components             */
-
-	/* znxwy  = (xb - ya, zb - wa, wd - zc, yd - xc) */
-
-	hpmquatf ZnXWY = _mm_sub_ps(_mm_mul_ps(*larg, baba), _mm_mul_ps(wzyx, dcdc));
-
-	/* xzynw  = (xd + yc, zd + wc, wb + za, yb + xa) */
-	hpmquatf XZYnW = _mm_add_ps(_mm_mul_ps(*larg, dcdc), _mm_mul_ps(wzyx, baba));
-
-	/* _mm_shuffle_ps(XZYnW, ZnXWY, _MM_SHUFFLE(3,2,1,0)) */
-	/*      = (xd + yc, zd + wc, wd - zc, yd - xc)        */
-	/* _mm_shuffle_ps(ZnXWY, XZYnW, _MM_SHUFFLE(2,3,0,1)) */
-	/*      = (zb - wa, xb - ya, yb + xa, wb + za)        */
-
-	/* _mm_addsub_ps adds elements 1 and 3 and subtracts elements 0 and 2, so we get: */
-	/* _mm_addsub_ps(*, *) = (xd+yc-zb+wa, xb-ya+zd+wc, wd-zc+yb+xa, yd-xc+wb+za)     */
-	/*
-	__m128 XZWY = _mm_addsub_ps(_mm_shuffle_ps(XZYnW, ZnXWY, _MM_SHUFFLE(3,2,1,0) ),
-								_mm_shuffle_ps(ZnXWY, XZYnW, _MM_SHUFFLE(2,3,0,1) ));
-	*/
-
-	hpmquatf XZWY = _mm_add_ps(_mm_shuffle_ps(XZYnW, ZnXWY, _MM_SHUFFLE(3,2,1,0) ),
-								_mm_shuffle_ps(ZnXWY, XZYnW, _MM_SHUFFLE(2,3,0,1) ));
-	XZWY = _mm_sub_ps(XZWY,
-								_mm_shuffle_ps(ZnXWY, XZYnW, _MM_SHUFFLE(2,3,0,1) ));
+	const hpmquatf wzyx = _mm_shuffle_ps(*larg, *larg, _MM_SHUFFLE(0,1,2,3) );		/*	{ w, x, y, z } => { w, z, y, x }	*/
+	const hpmquatf baba = _mm_shuffle_ps(*rarg, *rarg, _MM_SHUFFLE(0,1,0,1) );		/*	{ w, x, y, z } => { w, z, w, z }	*/
+	const hpmquatf dcdc = _mm_shuffle_ps(*rarg, *rarg, _MM_SHUFFLE(2,3,2,3) );		/*	{ w, x, y, z } => { y, z, y, z }		*/
+	const hpmquatf row1 = {-1,  1, -1,  1};
+	const hpmquatf row2 = {-1,  1,  1, -1};
+	const hpmquatf row3 = {-1, -1,  1,  1};
 
 
-	/* now we only need to shuffle the components in place and return the result      */
-	*out = _mm_shuffle_ps(XZWY, XZWY, _MM_SHUFFLE(2,1,3,0));
+	const hpmquatf qwwwwrwxyz = _mm_mul_ps(
+			_mm_shuffle_ps(*larg, *larg, _MM_SHUFFLE(0,0,0,0)),
+			_mm_shuffle_ps(*rarg, *rarg, _MM_SHUFFLE(0,1,2,3)));
+
+	const hpmquatf qxxxxrxwzy = _mm_mul_ps(
+			_mm_shuffle_ps(*larg, *larg,_MM_SHUFFLE(1,1,1,1)),
+			_mm_shuffle_ps(*rarg, *rarg,_MM_SHUFFLE(1,0,3,2))) * row1;
+
+	const hpmquatf qyyyyrxyzw = _mm_mul_ps(
+			_mm_shuffle_ps(*larg, *larg,_MM_SHUFFLE(2,2,2,2)),
+			_mm_shuffle_ps(*rarg, *rarg, _MM_SHUFFLE(1,2,0,1))) * row2;
+
+	const hpmquatf qzzzzrzyxw = _mm_mul_ps(
+			_mm_shuffle_ps(*larg, *larg,_MM_SHUFFLE(3,3,3,3)),
+			_mm_shuffle_ps(*rarg, *rarg,_MM_SHUFFLE(3,2,1,0))) * row3;
+
+	*out =  qwwwwrwxyz + qxxxxrxwzy + qyyyyrxyzw + qzzzzrzyxw;
 }
+
 
 HPM_IMP( void, hpm_quat_multi_vec3fv, const hpmquatf* larg, const hpmvec3f* rarg, hpmquatf* out){
 
@@ -55,5 +47,6 @@ HPM_IMP( void, hpm_quat_multi_vec3fv, const hpmquatf* larg, const hpmvec3f* rarg
 	const hpmquatf lzzxy = _mm_shuffle_ps(*larg, *larg, _MM_SHUFFLE(3, 3, 1, 2));	/*	{ w, x, y, z } => { z, z, x, y }	*/
 	const hpmquatf rzyzx = _mm_shuffle_ps(*rarg, *rarg, _MM_SHUFFLE(3, 2, 3, 1));	/*	{ w, x, y, z } => { z, y, z, x }	*/
 
+	/*	{ x^2 + y^2 + z^2, x*x + y*z + z*y, w*y + z*x + x*z, w*z + x*y + y*x }.	*/
 	*out = (lxwww * rxxyz) + (lyyzx * ryzxy) + (lzzxy * rzyzx);
 }
