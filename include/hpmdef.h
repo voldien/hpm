82f2b41aa8be45422ed576912558610d54d26dee
voldie <voldiekami@gmail.com>
2017-07-31 20:31:10 +0200

    Minor code cleanup.

--- include/hpmdef.h
+++ include/hpmdef.h
@@ -348,19 +348,19 @@ ret HPM_FUNCSYMBOLNAME(func)(__VA_ARGS__)		\
 #define _HPM_MATH_H_
 #include<math.h>
 #ifdef _HPM_MATH_H_
-	#define HPM_PI				3.14159265358979323846  	/* pi */
-	#define HPM_PI_2			1.57079632679489661923  	/* pi/2 */
-	#define HPM_PI_4			0.78539816339744830962  	/* pi/4 */
-	#define HPM_E				2.7182818284590452354   	/* e */
-	#define HPM_LOG2E			1.4426950408889634074   	/* log_2 e */
-	#define HPM_LOG10E			0.43429448190325182765  	/* log_10 e */
-	#define HPM_LN2           	0.69314718055994530942  	/* log_e 2 */
-	#define HPM_LN10          	2.30258509299404568402  	/* log_e 10 */
-	#define HPM_1_PI          	0.31830988618379067154  	/* 1/pi */
-	#define HPM_2_PI          	0.63661977236758134308  	/* 2/pi */
-	#define HPM_2_SQRTPI      	1.12837916709551257390  	/* 2/sqrt(pi) */
-	#define HPM_SQRT2         	1.41421356237309504880  	/* sqrt(2) */
-	#define HPM_SQRT1_2       	0.70710678118654752440  	/* 1/sqrt(2) */
+	#define HPM_PI              3.14159265358979323846  	/* pi */
+	#define HPM_PI_2            1.57079632679489661923  	/* pi/2 */
+	#define HPM_PI_4            0.78539816339744830962  	/* pi/4 */
+	#define HPM_E               2.7182818284590452354   	/* e */
+	#define HPM_LOG2E           1.4426950408889634074   	/* log_2 e */
+	#define HPM_LOG10E          0.43429448190325182765  	/* log_10 e */
+	#define HPM_LN2             0.69314718055994530942  	/* log_e 2 */
+	#define HPM_LN10            2.30258509299404568402  	/* log_e 10 */
+	#define HPM_1_PI            0.31830988618379067154  	/* 1/pi */
+	#define HPM_2_PI            0.63661977236758134308  	/* 2/pi */
+	#define HPM_2_SQRTPI        1.12837916709551257390  	/* 2/sqrt(pi) */
+	#define HPM_SQRT2           1.41421356237309504880  	/* sqrt(2) */
+	#define HPM_SQRT1_2         0.70710678118654752440  	/* 1/sqrt(2) */
 #endif	/*	_HPM_MATH_H_	*/
 
 
