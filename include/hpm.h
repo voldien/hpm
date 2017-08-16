a2234bd7734fc49ffaceca339f203ac14b8d0aac
voldie <voldiekami@gmail.com>
2017-08-16 15:23:59 +0200

    Minor cleanup.

--- include/hpm.h
+++ include/hpm.h
@@ -50,22 +50,22 @@ typedef int (HPMAPIENTRY PHPMSUPPORTCPUFEAT)(unsigned int simd);
 /**
  *	SIMD extensions flags.
  */
-#define HPM_NONE		(0 << 0)	/*	No HPM SIMD exention flag.	*/
-#define HPM_NOSIMD		(1 << 0)	/*	No SIMD hardware feature, using the FPU and ALU.	*/
-#define HPM_MMX			(1 << 1)	/*	MMX. (Yet not supported)	*/
-#define HPM_SSE			(1 << 2)	/*	SSE (Streaming SIMD Extensions).	*/
-#define HPM_SSE2		(1 << 3)	/*	SSE2 (Streaming SIMD Extensions).	*/
-#define HPM_SSE3		(1 << 4)	/*	SSE3 (Streaming SIMD Extensions).	*/
-#define HPM_SSSE3		(1 << 5)	/*	SSSE3 (Streaming SIMD Extensions).	*/
-#define HPM_SSE4_1		(1 << 6)	/*	SSE4.1 (Streaming SIMD Extensions).	*/
-#define HPM_SSE4_2		(1 << 7)	/*	SSE4.2 (Streaming SIMD Extensions).	*/
-#define HPM_AVX			(1 << 8)	/*	AVX Version 1 (Advanced Vector Extension).	*/
-#define HPM_AVX2		(1 << 9)	/*	AVX Version 2 (Advanced Vector Extension).	(Not tested)*/
-#define HPM_AVX512		(1 << 10)	/*	AVX512 (Advanced Vector Extension). (Yet not supported)	*/
-#define HPM_NEON		(1 << 11)	/*	ARM	FPU (floating-point unit) feature.	*/
-#define HPM_SVML		(1 << 29)	/*	Not supported.	*/
-#define HPM_ACML		(1 << 30)	/*	Not supported.	*/
-#define HPM_DEFAULT		(1 << 31)	/*	Default, makes attempt to take the best SIMD extension on the system.	*/
+#define HPM_NONE        (0 << 0)	/*	No HPM SIMD exention flag.	*/
+#define HPM_NOSIMD      (1 << 0)	/*	No SIMD hardware feature, using the FPU and ALU.	*/
+#define HPM_MMX         (1 << 1)	/*	MMX. (Yet not supported)	*/
+#define HPM_SSE         (1 << 2)	/*	SSE (Streaming SIMD Extensions).	*/
+#define HPM_SSE2        (1 << 3)	/*	SSE2 (Streaming SIMD Extensions).	*/
+#define HPM_SSE3        (1 << 4)	/*	SSE3 (Streaming SIMD Extensions).	*/
+#define HPM_SSSE3       (1 << 5)	/*	SSSE3 (Streaming SIMD Extensions).	*/
+#define HPM_SSE4_1      (1 << 6)	/*	SSE4.1 (Streaming SIMD Extensions).	*/
+#define HPM_SSE4_2      (1 << 7)	/*	SSE4.2 (Streaming SIMD Extensions).	*/
+#define HPM_AVX         (1 << 8)	/*	AVX Version 1 (Advanced Vector Extension).	*/
+#define HPM_AVX2        (1 << 9)	/*	AVX Version 2 (Advanced Vector Extension).	(Not tested)*/
+#define HPM_AVX512      (1 << 10)	/*	AVX512 (Advanced Vector Extension). (Yet not supported)	*/
+#define HPM_NEON        (1 << 11)	/*	ARM	FPU (floating-point unit) feature.	*/
+#define HPM_SVML        (1 << 29)	/*	Not supported.	*/
+#define HPM_ACML        (1 << 30)	/*	Not supported.	*/
+#define HPM_DEFAULT     (1 << 31)	/*	Default, makes attempt to take the best SIMD extension on the system.	*/
 
 
 /**
