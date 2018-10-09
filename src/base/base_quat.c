5e351cb6c1e0c4f74792e0476d251b4e51ab4c36
Voldie <voldiekami@gmail.com>
2018-10-09 09:27:50 +0200

    Updated SIMD include.

--- src/base/base_quat.c
+++ src/base/base_quat.c
@@ -2,10 +2,13 @@
 #include"hpmmatrix.h"
 
 #include<immintrin.h>
+
 #   ifdef HPM_VC
 #      include<intrin.h>
 #	elif defined(HPM_GNUC) || defined(HPM_CLANG)
-#		include<x86intrin.h>
+#       if defined(HPM_X86) || defined(HPM_X86_64)
+#		    include<x86intrin.h>
+#       endif
 #   endif
 
 
