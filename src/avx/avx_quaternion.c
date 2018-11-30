dc01038d2876f8e6e8b1fb273670452abf5de870
Voldie <voldiekami@gmail.com>
2018-11-30 10:54:54 +0100

    Updated quaternion vector mulitplication.

--- src/avx/avx_quaternion.c
+++ src/avx/avx_quaternion.c
@@ -41,15 +41,17 @@ HPM_IMP(void, hpm_quat_multi_quatfv, const hpmquatf* larg, const hpmquatf* rarg,
 
 }
 
-HPM_IMP( void, hpm_quat_multi_vec3fv, const hpmquatf* larg, const hpmvec3f* rarg, hpmquatf* out){
+HPM_IMP( void, hpm_quat_multi_vec3fv, const hpmquatf* quat, const hpmquatf* rf_vec, hpmvec3f* vec){
+	hpmvec3f t;
+	hpmvec4f q_xyz;
+	hpmquatf tcross;
 
-	const hpmquatf lxwww = _mm_shuffle_ps(*larg, *larg, _MM_SHUFFLE(1, 0, 0, 0));	/*	{ w, x, y, z } => { x, w, w, w }	*/
-	const hpmquatf rxxyz = _mm_shuffle_ps(*rarg, *rarg, _MM_SHUFFLE(1, 1, 2, 3));	/*	{ w, x, y, z } => { x, x, y, z }	*/
-	const hpmquatf lyyzx = _mm_shuffle_ps(*larg, *larg, _MM_SHUFFLE(2, 2, 3, 1));	/*	{ w, x, y, z } => { y, y, z, x }	*/
-	const hpmquatf ryzxy = _mm_shuffle_ps(*rarg, *rarg, _MM_SHUFFLE(2, 3, 1, 2));	/*	{ w, x, y, z } => { y, z, x, y }	*/
-	const hpmquatf lzzxy = _mm_shuffle_ps(*larg, *larg, _MM_SHUFFLE(3, 3, 1, 2));	/*	{ w, x, y, z } => { z, z, x, y }	*/
-	const hpmquatf rzyzx = _mm_shuffle_ps(*rarg, *rarg, _MM_SHUFFLE(3, 2, 3, 1));	/*	{ w, x, y, z } => { z, y, z, x }	*/
+	/*  Compute t.  */
+	HPM_CALLLOCALFUNC(hpm_vec4_setf)(&q_xyz, hpm_quat_getxf(*quat), hpm_quat_getyf(*quat), hpm_quat_getzf(*quat), 0.0f);
+	HPM_CALLLOCALFUNC(hpm_vec3_crossproductfv)( &q_xyz, rf_vec,  &t);
+	t *= 2.0f;
 
-	/*	{ x^2 + y^2 + z^2, x*x + y*z + z*y, w*y + z*x + x*z, w*z + x*y + y*x }.	*/
-	*out = (lxwww * rxxyz) + (lyyzx * ryzxy) + (lzzxy * rzyzx);
+	/*  Compute rotated vector. */
+	HPM_CALLLOCALFUNC(hpm_vec3_crossproductfv)( &q_xyz, &t,  &tcross);
+	*vec = *rf_vec + hpm_quat_getwf(*quat) * t + tcross;
 }
