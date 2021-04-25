021c7ec85513c811a9769e49b732886672fd9616
Valdemar Lindberg <voldiekami@gmail.com>
2021-04-25 16:26:30 +0200

    moved include

--- src/base/base_quat.c
+++ src/base/base_quat.c
@@ -1,13 +1,12 @@
 #include"hpmquaternion.h"
 #include"hpmmatrix.h"
+#include <assert.h>
 
 #   ifdef HPM_VC
 #      include<intrin.h>
 #	elif defined(HPM_GNUC) || defined(HPM_CLANG)
 #       if defined(HPM_X86) || defined(HPM_X86_64)
 #		    include<x86intrin.h>
-#include <assert.h>
-
 #       endif
 #   endif
 
